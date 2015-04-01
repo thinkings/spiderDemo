34b87d7d7c2fe1655df160154e75da930b8a3af6
tim <981834307@qq.com>
2015-04-01 17:18:07 +0800

    spider demo
    Signed-off-by: tim

---
+++ src/main/java/com/thinking/demo/spiderDemo/App.java
@@ -0,0 +1,13 @@
+package com.thinking.demo.spiderDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
